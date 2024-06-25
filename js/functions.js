d3ae2f855be2ccb2676afcdf629eb246c3ca89cf
Andrey <andreonof@yandex.ru>
2024-06-25 22:18:49 +0300

    Закомментированы все выводы в консоль

--- js/functions.js
+++ js/functions.js
@@ -6,9 +6,9 @@ function stringAnalysis(contentString, maxLength){
   return contentString.length <= maxLength;
 }
 /*Выводя в консоль, чтобы отобразилось true или false (больше или меньше параметр contentString и maxLength)*/
-console.log(stringAnalysis('проверяемая строка', 20));
-console.log(stringAnalysis('проверяемая строка', 18));
-console.log(stringAnalysis('проверяемая строка', 10));
+// console.log(stringAnalysis('проверяемая строка', 20));
+// console.log(stringAnalysis('проверяемая строка', 18));
+// console.log(stringAnalysis('проверяемая строка', 10));
 // Проверка на паллиндром
 function pallindrom(string){
   const cleaned = string.toLowerCase().replaceAll(' ', '');
@@ -16,6 +16,6 @@ function pallindrom(string){
   for (let i = cleaned.length - 1; i >= 0; i--){
     reversed = reversed + cleaned[i];
   }
-  console.log('string === reversed')
+  // console.log('string === reversed')
   return string === reversed;
 }
