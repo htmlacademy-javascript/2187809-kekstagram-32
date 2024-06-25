10805eecfa0dcee5bcfc330ed51885cdb46de313
Andrey <andreonof@yandex.ru>
2024-06-25 22:29:48 +0300

    Проверка на паллидром готова!!!

--- js/functions.js
+++ js/functions.js
@@ -5,17 +5,11 @@
 function stringAnalysis(contentString, maxLength){
   return contentString.length <= maxLength;
 }
-/*Выводя в консоль, чтобы отобразилось true или false (больше или меньше параметр contentString и maxLength)*/
-// console.log(stringAnalysis('проверяемая строка', 20));
-// console.log(stringAnalysis('проверяемая строка', 18));
-// console.log(stringAnalysis('проверяемая строка', 10));
-// Проверка на паллиндром
-function pallindrom(string){
-  const cleaned = string.toLowerCase().replaceAll(' ', '');
-  let reversed = '';
-  for (let i = cleaned.length - 1; i >= 0; i--){
-    reversed = reversed + cleaned[i];
+const palindromeTest = (string) => {
+  const standardString = string.replaceAll(' ', '').toLowerCase();
+  let reflectionString = '';
+  for (let i = standardString.length - 1; i >= 0; i--) {
+    reflectionString += standardString[i];
   }
-  // console.log('string === reversed')
-  return string === reversed;
+  return standardString === reflectionString;
 }
