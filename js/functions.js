7d6c3a6e5a5537933e6406843e4e540ca9aa52c6
Andrey <andreonof@yandex.ru>
2024-06-25 22:15:09 +0300

    Усовершенствование кода

--- js/functions.js
+++ js/functions.js
@@ -11,11 +11,10 @@ console.log(stringAnalysis('проверяемая строка', 18));
 console.log(stringAnalysis('проверяемая строка', 10));
 // Проверка на паллиндром
 function pallindrom(string){
-  string = string.toLwerCase();
-  string = string.replaceAll(' ', '');
+  const cleaned = string.toLowerCase().replaceAll(' ', '');
   let reversed = '';
-  for (let i = string.length - 1; i >= 0; i--){
-    reversed = reversed + reversed[i];
+  for (let i = cleaned.length - 1; i >= 0; i--){
+    reversed = reversed + cleaned[i];
   }
   console.log('string === reversed')
   return string === reversed;
