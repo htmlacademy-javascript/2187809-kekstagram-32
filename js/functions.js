6244dacf41e796383e451492266054d6bb2b694a
Andrey <andreonof@yandex.ru>
2024-06-25 22:04:51 +0300

    Добавлена проверка на паллиндром

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,14 @@ function stringAnalysis(contentString, maxLength){
 console.log(stringAnalysis('проверяемая строка', 20));
 console.log(stringAnalysis('проверяемая строка', 18));
 console.log(stringAnalysis('проверяемая строка', 10));
+// Проверка на паллиндром
+function pallindrom(string){
+  string = string.toLwerCase();
+  string = string.replaceAll(' ', '');
+  let reversed = '';
+  for (let i = string.length - 1; i >= 0; i--){
+    reversed = reversed + reversed[i];
+  }
+  console.log('string === reversed')
+  return string === reversed;
+}
